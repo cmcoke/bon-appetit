83de12777b0d2e0873ea693d3381d44597c1c35b
Charles Coke <minoru.cm@gmail.com>
2018-06-24 22:37:02 -0500

    Completed reviews section

--- app/assets/scripts/vendor.js
+++ app/assets/scripts/vendor.js
@@ -1,3 +1,5 @@
 import '../../temp/scripts/modernizr';
 import 'picturefill';
 import 'lazysizes';
+import 'flickity';
+import 'flickity-imagesloaded';
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -60,15 +60,17 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 7:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
